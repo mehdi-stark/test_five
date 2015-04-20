44122d27048dbf248654c13b64d54dc62834c132
Mehdi Stark <mehdi.stark@gmail.com>
2015-04-20 12:02:26 +0200

    rendu 3

--- Test_Five/js/app.js
+++ Test_Five/js/app.js
@@ -1,1 +1,14 @@
-﻿var app = angular.module('myMoments');
+﻿var app = angular.module('myMoments', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/allMoments.html',
+            controller : 'allMomentsController'
+        })
+        .when('/', {
+            templateUrl: 'partials/addMoments.html',
+            controller: 'addMomentsController'
+        })
+
+});
