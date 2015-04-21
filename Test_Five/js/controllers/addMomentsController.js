06733918c5d67e8f46b484629812c46e9110ae16
Mehdi Stark <mehdi.stark@gmail.com>
2015-04-21 15:50:00 +0200

    rendu

--- Test_Five/js/controllers/addMomentsController.js
+++ Test_Five/js/controllers/addMomentsController.js
@@ -1,3 +1,35 @@
 ﻿app.controller('addMomentsController', function ($scope) {
-    //
+   
+    $scope.moment = {
+        image: '',
+        title: ''
+    };
+
+    $scope.moments = [];
+
+    $scope.addMoment = function () {
+        $scope.moments.push($scope.moment);
+        $scope.save();
+    };
+
+    $scope.save = function () {
+        requestFileSystem(window.PERSISTENT, 0, fsReceived, errorHandler);
+    };
+
+    var fsReceived = function (fileSystem) {
+        fileSystem.root.getFile('moments.json', { create: true, exclusive, false}, feReceived, errorHandler);
+    };
+
+    var feReceived = function (fileEntry) {
+        fileSystem.createWriter(fwReceived, errorHandler);
+    };
+
+    var fwReceived = function (fileWriter) {
+        var momentsText = angular.toJson($scope.moments);
+        fileWriter.write(momentsText);
+    };
+
+    var errorHandler = function (error) {
+        print ("ERREUR ADDMOMENT CONTROLLER\n");
+    };
 });
